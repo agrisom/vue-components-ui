d4dd8418eb1d3463adb064954e76eea0eb624e9d
agm0637 <albertgrisomendez@mgs.es>
2022-02-08 18:34:06 +0100

    Init component library with vue-sfc-rollup

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { VueComponentsUiSample } from '@/entry.esm';
+
+export default defineComponent({
+  name: 'ServeDev',
+  // components: {
+  //  VueComponentsUiSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-components-ui-sample />
+  </div>
+</template>
---
+++ src/lib-components/index.ts
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as VueComponentsUiSample } from './vue-components-ui-sample.vue';
