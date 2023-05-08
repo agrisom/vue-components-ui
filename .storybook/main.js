ca261e1d3a0b787f0d0af94933edb30d755ed829
agm0637 <albertgrisomendez@mgs.es>
2023-05-08 13:56:54 +0200

    Upgrade dependency versions

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,45 +1,30 @@
 const { resolve } = require('path');
 
-module.exports = {  
-  webpackFinal: async (config) => {
+module.exports = {
+  webpackFinal: async config => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': path.resolve(__dirname, "src"),
+      '@': path.resolve(__dirname, "src")
     };
     config.module.rules.push({
       test: /\.scss$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        {
-          loader: "sass-loader",
-          options: {
-            additionalData: (content) => {
-              const LF = '\n';
-              const paths = [
-                '@use \'sass:math\';',
-                '@use \'@/shared/style/style.scss\';',
-              ];
-              return paths.join(LF) + LF + content;
-            },
-            implementation: require('sass'),
+      use: ['style-loader', 'css-loader', {
+        loader: "sass-loader",
+        options: {
+          additionalData: content => {
+            const LF = '\n';
+            const paths = ['@use \'sass:math\';', '@use \'@/shared/style/style.scss\';'];
+            return paths.join(LF) + LF + content;
           },
-        },
-      ],
-      include: resolve(__dirname, '../'),
+          implementation: require('sass')
+        }
+      }],
+      include: resolve(__dirname, '../')
     });
     return config;
   },
-  "stories": [
-    "../src/docs/*.mdx",
-    "../src/components/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "storybook-addon-sass-postcss"
-  ],
+  "stories": ["../src/docs/Introduction.stories.mdx", "../src/docs/*.mdx", "../src/components/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "storybook-addon-sass-postcss"],
   "framework": {
     "name": "@storybook/vue3-vite",
     "options": {}
@@ -47,4 +32,4 @@ module.exports = {
   "docs": {
     "autodocs": "tag"
   }
-}
+};
