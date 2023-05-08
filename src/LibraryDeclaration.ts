0e51b355f540b219f6e0ef1208d092f124fd520c
agm0637 <albertgrisomendez@mgs.es>
2023-05-08 14:46:57 +0200

    Export lists

--- src/LibraryDeclaration.ts
+++ src/LibraryDeclaration.ts
@@ -3,3 +3,6 @@ export { default as UILoader } from './components/UILoader/UILoader.vue';
 
 export type { Size, SizeX } from './shared/types/size.type';
 export type { Icon } from './shared/types/icon.type';
+
+export { listSize, listSizeX } from './shared/types/size.type';
+export { listIcon } from './shared/types/icon.type';
